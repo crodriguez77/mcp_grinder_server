9e9bb09fb4b884ca9fc702a38f61094519e5a89f
dreck7 <dreck7@gmail.com>
2025-10-11 20:58:43 -0600

    CodeDeploy deployment manual test.

--- mcp_grinder_server/server.py
+++ mcp_grinder_server/server.py
@@ -22,7 +22,7 @@ class ResourceServerSettings(BaseSettings):
         super().__init__(**data)
 
 def main():
-    print("Hello from mcp-grinder-server!")
+    print("Hello from mcp-grinder-server - CodeDeploy Manual Test!")
     s = source_mod.customSource()
     pass_mod.passThrough(s)
 
